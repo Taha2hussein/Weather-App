c079bd008a1464fd52135c3ec83f3f2f885168e6
Taha Hussein <taha.hussein.two@gmail.com>
2025-04-17 13:57:22 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Taha Hussein on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
